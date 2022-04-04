4c5557a4f42d2a51bc1b612071ccea7cbc57eb0d
Justin Conger <jhconger@GMAIL.COM>
2022-04-04 09:34:56 -0500

    adding Author Album and Quote beans

---
+++ src/main/java/bean/Author.java
@@ -0,0 +1,17 @@
+package bean;
+
+public class Author {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Author() {};
+
+    public Author(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
